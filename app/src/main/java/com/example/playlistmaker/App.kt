d76226eafc91dcd0538d551b3c99843ddfe81ddc
Dmitriy <hoddlep@yandex.ru>
2024-11-28 11:16:15 +0500

    классы перенесены по пакетам применяя принцыпы чистой архитектуры

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -4,7 +4,7 @@ import android.app.Application
 
 class App : Application() {
 
-    private val settingsInteractor by lazy { Creator.provideSettingsInteractor() }
+    private val settingsInteractor = Creator.provideSettingsInteractor()
 
     override fun onCreate() {
         super.onCreate()
