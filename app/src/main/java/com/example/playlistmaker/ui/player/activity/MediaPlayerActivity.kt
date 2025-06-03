e1b3d7307e89c1f72885e94bd48c76fed40f14fe
Dmitriy <hoddlep@yandex.ru>
2025-06-03 10:53:58 +0500

    Удалены лишние зависимости

--- app/src/main/java/com/example/playlistmaker/ui/player/activity/MediaPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/player/activity/MediaPlayerActivity.kt
@@ -2,7 +2,6 @@ package com.example.playlistmaker.ui.player.activity
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
